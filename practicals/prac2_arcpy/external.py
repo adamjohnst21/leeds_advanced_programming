f9230bf07f8f955e0368cb8dec1c00e142731854
adamjohnst21 <adamjohnst21@gmail.com>
2018-05-07 20:45:39 +0100

    website commit

--- practicals/prac2_arcpy/external.py
+++
@@ -1,3 +0,0 @@
-import arcpy
-
-arcpy.ImportFromE00_conversion("C:/Users/University/Dropbox/University/Advanced_programming/practicals/prac1_modelBuilder/data/build.e00", "C:/Users/University/Dropbox/University/Advanced_programming/practicals/prac1_modelBuilder/data", "build2")
